6b995d9013d3eb4e72f797abe85b396c40ad0083
YOSHITAKA Matsumoto <yo4taka.ma2moto@gmail.com>
2025-11-20 11:25:30 +0900

    エラー修正

--- go/excellent/main.go
+++ go/excellent/main.go
@@ -1,7 +1,6 @@
 package main
 
-func EvenOrOdd(number int)
-string {
+func EvenOrOdd(number int) string {
 	if number%2 == 0 {
 		return "Even"
 	} else {
--- go/excellent/main_test.go
+++ go/excellent/main_test.go
@@ -2,8 +2,7 @@ package main
 
 import "testing"
 
-func TestEvenOrOdd(t *testing.T)
-{
+func TestEvenOrOdd(t *testing.T) {
 	result := EvenOrOdd(10)
 	if result != "Even" {
 		t.Errorf("expected: even, actual: %s", result)
